c9384e1a242c9fc6ad3764e4b200295cc8a1f331
OndraMurina <ondramurina@gmail.com>
2025-12-03 17:07:07 +0100

    pokus o sprovozneni

--- Olympiada/build/index.js
+++ Olympiada/build/index.js
@@ -1,8 +1,10 @@
 import { createRobutek } from "./libs/robutek.js";
 import * as colors from "./libs/colors.js";
 import { LED_WS2812B, SmartLed } from "smartled";
+import * as gpio from "gpio";
 const robutek = createRobutek("V2");
-const ledStrip = new SmartLed(robutek.Pins.ILED, 7, LED_WS2812B); // robutek.Pins.ILED je pin 48
+const thresh = 600; // Mez pod kterou se barva považuje za černou
+const ledStrip = new SmartLed(robutek.Pins.ILED, 7, LED_WS2812B);
 ledStrip.set(0, colors.white);
 ledStrip.set(1, colors.white);
 ledStrip.set(2, colors.white);
@@ -11,33 +13,48 @@ ledStrip.set(4, colors.white);
 ledStrip.set(5, colors.white);
 ledStrip.set(6, colors.white);
 ledStrip.show();
-const setpoint = 512;
-let speed = 789;
-let k_p = 0.96;
-let k_d = 4.86;
-function move(steering, speed) {
-    if (steering < 0) {
-        robutek.leftMotor.setSpeed((1 + steering) * speed);
-        robutek.rightMotor.setSpeed(speed);
+let on = 0;
+gpio.pinMode(robutek.Pins.ButtonRight, gpio.PinMode.INPUT);
+gpio.on("falling", robutek.Pins.ButtonRight, () => {
+    let on = 1;
+});
+gpio.pinMode(robutek.Pins.ButtonRight, gpio.PinMode.INPUT);
+gpio.on("falling", robutek.Pins.ButtonLeft, () => {
+    let on = 0;
+});
+while (on = 1) {
+    const l = robutek.readSensor("LineFL");
+    const r = robutek.readSensor("LineFR");
+    if (l < thresh) {
+        robutek.leftMotor.setSpeed(100);
+        robutek.rightMotor.setSpeed(10);
     }
-    else if (steering > 0) {
-        robutek.rightMotor.setSpeed((1 - steering) * speed);
-        robutek.leftMotor.setSpeed(speed);
+    else if (r < thresh) {
+        robutek.leftMotor.setSpeed(10);
+        robutek.rightMotor.setSpeed(100);
     }
+    else {
+        robutek.leftMotor.setSpeed(100);
+        robutek.leftMotor.setSpeed(100);
+    }
+    await sleep(10);
+    console.log(`l: ${l}, r: ${r}`);
 }
-async function main() {
-    let previous_error = 0;
-    robutek.leftMotor.move();
-    robutek.rightMotor.move();
-    console.log("start");
-    while (true) {
-        const l = robutek.readSensor("LineFR");
-        let error = setpoint - l;
-        let normalized_error = error / 512;
-        let speed_of_change = normalized_error - previous_error;
-        move(normalized_error * k_p + speed_of_change * k_d, speed);
-        previous_error = normalized_error;
-        await sleep(1);
-    }
-}
-main().catch(console.error);
+// do příště si chci udělat tlačítko, co spusti a zastaví program
+// komentare: minimum a maximum speed konstanta, hodnoty pojmenovany, 
+// vypsat co se srobotem deje(vzpis senzoru) 
+// sledovat rychlost kol pomoci wifi
+/* zastaveni if
+import * as gpio from "gpio";
+gpio.pinMode(robutek.Pins.ButtonRight, gpio.PinMode.INPUT);
+
+gpio.on("falling", robutek.Pins.ButtonRight, () => {
+  robutek.stop()
+});
+
+zarovnani control shift F
+zapoznamkovani ctrl K C
+yastaven9 terminalu ctrl c
+shift a tabulator nebo jen tabulator cely texty dolevanebo doprava
+*/
+//prohodit l a r v while true
